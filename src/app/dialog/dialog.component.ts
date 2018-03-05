0442af58e62e20b747b12ca76d402b967acdc178
rouz majlessi <rmajlessi@gmail.com>
2018-03-04 19:21:11 -0800

    Add Dialog component

---
+++ src/app/dialog/dialog.component.ts
@@ -0,0 +1,25 @@
+import { Component, ViewEncapsulation, ViewChild } from '@angular/core';
+import { EJComponents } from 'ej-angular2';
+
+@Component({
+  selector: 'ej-app',
+  templateUrl: './dialog.component.html'
+})
+export class DialogComponent {
+   resize: boolean;
+   button_display: boolean;
+   @ViewChild('dialog') dialog: EJComponents <any,any>;
+   constructor() {
+    this.resize = false;
+    this.button_display = false;
+  }
+  //Button click event handler to open the ejDialog
+  onClick(event) {
+   this.button_display = false;
+   this.dialog.widget.element.ejDialog('open');
+  }
+  //Dialog close event handler
+  onClose(event) {
+    this.button_display = true;
+  }
+}
